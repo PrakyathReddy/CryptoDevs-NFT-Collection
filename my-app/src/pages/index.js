be4874af6bb4c034be9a13ca384bce78fd11a968
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-03-10 06:05:28 +0530

    working on front-end

---
+++ my-app/src/pages/index.js
@@ -0,0 +1,120 @@
+import { Contract, contract, providers, utils } from "ethers";
+import head from "next/head";
+import React, { useEffect, useRef, useState } from "react";
+import Web3Modal from "web3modal";
+import { abi, NFT_CONTRACT_ADDRESS } from "../constants";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [presaleStarted, setPresaleStarted] = useState(false);
+  const [presaleEnded, setPresaleEnded] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [isOwner, setIsOwner] = useState(false);
+  const [tokenIdsMinted, setTokenIdsMinted] = useState("0");
+  const web3ModalRef = useRef();
+
+  const presaleMint = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer);
+      const tx = await nftContract.presaleMint({
+        value: utils.parseEther("0.1"),
+      });
+      setLoading(true);
+      await tx.wait();
+      setLoading(false);
+      window.alert("You successfully mined a CryptoDev!");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const publicMint = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer);
+      const tx = await nftContract.mint({
+        value: utils.parseEther("0.1"),
+      });
+      setLoading(true);
+      await tx.wait();
+      setLoading(false);
+      window.alert("You successfully mined a CryptoDev!");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner(true);
+      setWalletConnected(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const startPresale = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer);
+      const tx = await nftContract.startPresale();
+      setLoading(true);
+      await tx.wait();
+      setLoading(false);
+      await checkIfPresaleStarted();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const checkIfPresaleStarted = async () => {
+    try {
+      const provider = await getProviderOrSigner(false);
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer);
+      const _presaleStarted = await nftContract.presaleStarted();
+      if (!_presaleStarted) {
+        await getOwner();
+      }
+      setPresaleStarted(_presaleStarted);
+      return _presaleStarted;
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  };
+
+  const checkIfPresaleEnded = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, provider);
+      const _presaleEnded = await nftContract.presaleEnded();
+      const hasEnded = _presaleEnded.lt(Math.floor(Date.now() / 1000));
+      if (hasEnded) {
+        setPresaleEnded(true);
+      } else {
+        setPresaleEnded(false);
+      }
+      return hasEnded;
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  };
+
+  const getOwner = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, provider);
+      const _owner = await nftContract.owner();
+      const signer = await getProviderOrSigner(true);
+      const address = await signer.getAddress();
+      if (address.toLowerCase() === _owner.toLowerCase()) {
+        setIsOwner(true);
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+}
